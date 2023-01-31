7e068017dce77897114c7377198fa43eb97affac
rijan adhikari <rijanadhikari@gmail.com>
2023-01-31 11:48:55 +0900

    added title

--- app/models/collection.py
+++ app/models/collection.py
@@ -3,11 +3,13 @@ from sqlalchemy import Column, Integer, String, SmallInteger
 
 from app.models.base import Base
 
+
 class Collection(Base):
     __tablename__ = 'collections'
 
     id = Column(Integer, primary_key=True, index=True)
     type = Column(SmallInteger, index=True, nullable=False)
     slug = Column(String, index=True, nullable=False)
+    title = Column(String, nullable=False)
     image = Column(String, nullable=False)
     description = Column(String, nullable=False)
