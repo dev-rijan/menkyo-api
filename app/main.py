1d9764633410b348ac32899e224f12f4a12c9f34
rijan adhikari <rijanadhikari@gmail.com>
2023-01-31 11:24:33 +0900

    cleanup

--- app/main.py
+++ app/main.py
@@ -5,11 +5,6 @@ from app.api.v1.api import api_router
 
 def create_app() -> FastAPI:
     app = FastAPI(title=settings.PROJECT_NAME)
-
-    @app.get("/")
-    async def root():
-        return {"message": "Hello World"}
-
     app.include_router(api_router)
 
     return app
