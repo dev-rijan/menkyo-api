e29378ba33be9ffb472110653fbe854f0af343d9
rijan adhikari <rijanadhikari@gmail.com>
2023-01-31 10:33:14 +0900

    added api dependencies

--- app/api/deps.py
+++ app/api/deps.py
@@ -1,13 +1,5 @@
 from typing import Generator
-
-from fastapi import Depends, HTTPException, status
-from pydantic import ValidationError
-from sqlalchemy.orm import Session
-
-from app import crud, models, schemas
-from app.core import security
-from app.core.config import settings
-from app.db.session import SessionLocal
+from app.database.session import SessionLocal
 
 
 def get_db() -> Generator:
